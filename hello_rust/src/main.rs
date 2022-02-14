c6bd8e21049509f88f7b4aa9efa0fc0aef076ad3
duinodu <472365351duino@gmail.com>
2022-02-14 15:18:20 +0800

    add aarch64 support

--- hello_rust/src/main.rs
+++ hello_rust/src/main.rs
@@ -1,19 +1,22 @@
+use opencv::{core, highgui, imgcodecs, imgproc, prelude::Mat, Result};
 use std::env;
-use opencv::{
-    highgui,
-    imgcodecs,
-    Result,
-
-};
-
 
 fn main() -> Result<()> {
-    let args : Vec<String> = env::args().collect();
+    let args: Vec<String> = env::args().collect();
     assert_eq!(args.len(), 2);
 
     let image = imgcodecs::imread(&args[1], 1)?;
-    highgui::named_window("opencv in rust", 0)?;
-    highgui::imshow("opencv in rust", &image)?;
-    highgui::wait_key(10000)?;
+
+    if cfg!(target_arch = "x86_64") {
+        highgui::named_window("opencv in rust", 0)?;
+        highgui::imshow("opencv in rust", &image)?;
+        highgui::wait_key(10000)?;
+    } else if cfg!(target_arch = "aarch64") {
+        let mut gray = Mat::default();
+        imgproc::cvt_color(&image, &mut gray, imgproc::COLOR_BGR2GRAY, 0)?;
+        let imwrite_flags = core::Vector::<i32>::new();
+        imgcodecs::imwrite("output_gray.jpg", &gray, &imwrite_flags)?;
+        println!("saved to output_gray.jpg");
+    }
     Ok(())
 }
